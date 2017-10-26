6d7da5ae9ad59272b383ed2e1247799283d61999
AlexKapl <bizizib@gmail.com>
2017-10-26 20:25:08 +0300

    front-end keep rolling

--- view/camagru.php
+++ view/camagru.php
@@ -3,14 +3,14 @@
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>Camagru</title>
 	<link rel="stylesheet" href="../style/style.css">
+	<title>Camagru</title>
 </head>
 <body>
 <header>
 	<nav class="container">
 		<div class="logo">
-			<img src="../images/logo1.png">
+			<img class="max" src="../images/logo1.png">
 		</div>
 		<div class="nav-toggle"><span></span></div>
 		<ul id="menu">
@@ -18,29 +18,32 @@
 				<a href="http://localhost:8080/profile">User Profile</a>
 			</li>
 			<li>
-				<a href="http://localhost:8080/logout">Logout</a>
+				<a href="http://localhost:8080/logout"><img src="../images/logout-24.gif"></a>
 			</li>
 		</ul>
 	</nav>
 </header>
 <?= $_SESSION['Message'] ?>
-<div class="container">
-	<div class="post_list">
-		<article id="post-1" class="post">
-			<div class="post-image"><a href=""><img src="https://html5book.ru/wp-content/uploads/2016/05/rasskaz_slovar_rodnoy_prirodi.jpg"></a></div>
-<!--			<div class="post-image"><a href=""><img src=""></a></div>-->
-			<div class="post-content">
-				WTF IS THIS??<br/>
-				<br/>
-				<div class="post-footer">
-					<div class="post-social">
-						<a href="" target="_blank"><i class="fa fa-facebook"></i></a>
-						<a href="" target="_blank"><i class="fa fa-twitter"></i></a>
-						<a href="" target="_blank"><i class="fa fa-pinterest"></i></a>
+<div class="wrapper">
+	<div class="container">
+		<div class="post_list">
+			<article id="post-1" class="post">
+				<div class="post-image"><a href=""><img class="max"
+														src="https://html5book.ru/wp-content/uploads/2016/05/rasskaz_slovar_rodnoy_prirodi.jpg"></a>
+				</div>
+				<div class="post-content">
+					WTF IS THIS??<br/>
+					<br/>
+					<div class="post-footer">
+						<div class="post-social">
+							<a href="" target="_blank"><i class="fa fa-facebook"></i></a>
+							<a href="" target="_blank"><i class="fa fa-twitter"></i></a>
+							<a href="" target="_blank"><i class="fa fa-pinterest"></i></a>
+						</div>
 					</div>
 				</div>
-			</div>
-		</article>
+			</article>
+		</div>
 	</div>
 </div>
 <footer>
@@ -50,10 +53,9 @@
 		</div>
 		<div class="footer-col">
 			<div class="social-bar-wrap">
-				<a title="Facebook" href="" target="_blank"><i class="fa fa-facebook"></i></a>
-				<a title="Twitter" href="" target="_blank"><i class="fa fa-twitter"></i></a>
-				<a title="Pinterest" href="" target="_blank"><i class="fa fa-pinterest"></i></a>
-				<a title="Instagram" href="" target="_blank"><i class="fa fa-instagram"></i></a>
+				<a title="Facebook" href="" target="_blank"><img src="../images/facebook-24.gif"></a>
+				<a title="LinkedIn" href="" target="_blank"><img src="../images/linkedin-24.gif"></a>
+				<a title="Instagram" href="" target="_blank"><img src="../images/instagram-24.gif"></a>
 			</div>
 		</div>
 		<div class="footer-col">
@@ -62,7 +64,7 @@
 	</div>
 </footer>
 <script>
-	document.getElementsByClassName('nav-toggle')[0].onclick = function(){
+	document.getElementsByClassName('nav-toggle')[0].onclick = function () {
 		document.getElementById('menu').classList.toggle('active');
 	}
 </script>
--- view/login.php
+++ view/login.php
@@ -6,44 +6,47 @@
 	<link rel="stylesheet" href="../style/style.css">
 	<link rel="stylesheet" href="../style/login.css">
 	<style>
-		form {
-			width: 25%;
-			height: 15%;
-		}
-
-		button {
-			margin-top: 20px;
-			width: 35%;
-			height: auto;
-			background-color: black;
-			font-size: 18px;
+		nav {
+			text-align: center;
 		}
 	</style>
 </head>
 <body>
+<header>
+	<nav class="container">
+		<div class="logo">
+			<img class="max" src="../images/logo1.png">
+		</div>
+	</nav>
+</header>
 <form class="form" action="login" method="POST">
 	<?= $_SESSION['Message'] ?>
-	<p>
-		<input type="text" name="login" required pattern="[\w\d]{6,}" placeholder="Login(min 6)">
-	</p>
-	<p>
-		<input type="password" name="password" required pattern="<?=pattern?>" placeholder="Password(min 6)">
-	</p>
-	<p>
-		<button name="do_login">
-			LogIn
-		</button>
-	</p>
-	<p>
-		<button class="signup" onclick='location.href="signup"'>
-			Register
-		</button>
-	</p>
-	<p>
-		<button class="signup" onclick='location.href="forgot"'>
-			Forgot password?
-		</button>
-	</p>
+	<input type="text" name="login" required pattern="[\w\d]{6,}" placeholder="Login(min 6)">
+	<br/>
+	<input type="password" name="password" required pattern="<?= pattern ?>" placeholder="Password(min 6)">
+	<br/>
+	<button name="do_login">LogIn</button>
+	<br/>
+	<button class="signup" onclick='location.href="signup"'>Register</button>
+	<br/>
+	<button class="signup" onclick='location.href="forgot"'>Forgot password?</button>
 </form>
+<footer>
+	<div class="container">
+		<div class="footer-col">
+			<span>Camagru © 2017</span>
+		</div>
+		<div class="footer-col">
+			<div class="social-bar-wrap">
+				<a title="Facebook" href="" target="_blank"><img src="../images/facebook-24.gif"></a>
+				<a title="LinkedIn" href="" target="_blank"><img src="../images/linkedin-24.gif"></a>
+				<a title="Instagram" href="" target="_blank"><img src="../images/instagram-24.gif"></a>
+			</div>
+		</div>
+		<div class="footer-col">
+			<a href="mailto:admin@yoursite.ru">Contact us</a>
+		</div>
+	</div>
+</footer>
 </body>
 </html>
